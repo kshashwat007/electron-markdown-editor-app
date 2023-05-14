80d107924e874ca052cabe0c763418778761555e
kshashwat007 <kshashwat007@gmail.com>
2023-05-14 13:41:02 +0530

    css,lint and prettier setup with base react app

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrorParens: 'avoid',
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none',
+};
+
+export default options;
